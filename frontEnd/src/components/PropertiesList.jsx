6f618e3adc8e7709af0d154253455a5a8f60cf83
Joseph Venable <JJVenable@gmail.com>
2021-12-21 15:01:52 -0500

    added background for all pages to match

--- frontEnd/src/components/PropertiesList.jsx
+++ frontEnd/src/components/PropertiesList.jsx
@@ -14,10 +14,10 @@ return (
   { props.propertiesData.map((propertiesData, index) => ( 
     <div key={index} className="property-card" >
       
-    <p>{propertiesData.location}</p>
-    <p>{propertiesData.owner}</p>
-    <p>{propertiesData.currentTenant}</p>
-    <p>{propertiesData.comments}</p>
+    <p>Location: {propertiesData.location}</p>
+    <p>Owner: {propertiesData.owner}</p>
+    <p>Current Tenant: {propertiesData.currentTenant}</p>
+    <p>Comments: {propertiesData.comments}</p>
     
   </div>
   ))} 
