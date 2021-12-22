5f11250e67ae90170f0cc89afe7de6baac194efb
Joseph Venable <JJVenable@gmail.com>
2021-12-21 23:30:43 -0500

    part way with edit.jsx- model after task/tasklist in the morning

--- frontEnd/src/components/Edit.jsx
+++ frontEnd/src/components/Edit.jsx
@@ -1,12 +1,39 @@
 import React from "react";
+import axios from "axios";
+import {useState} from 'react'
 
 function Edit(props) {
-  console.log(props);
+
+const [editForm, setEditForm] = useState({
+  comments:''
+})
+// const handleSubmit = 
+// async (e) => {
+//   (e).preventDefault()
+//   setEditForm(editForm)
+//   const res = await axios.put('http://localhost:3001/api/properties/IDPULLED', editForm);
+  
+//   console.log(logForm)
+//   // window.location.reload() 
+//   console.log("working")
+// }
+
+
   return (
     <div className='editPage'>
-      <div>
-      <p> Edit Page</p>
-      </div>
+      <h1>Edit comment below:</h1>
+      <form onSubmit={handleSubmit2} >
+    <input
+      type="text-area"
+      value={editForm.comments}
+      onChange={(e)=>{
+        setEditForm({...editForm, name: e.target.value})
+      }}
+      comments={"New Comment Text"}
+      placeholder={"NEED TO PULL DATA AND ENTER TO BE AWESOME"}
+    />
+      <button>Submit Comment Edit</button>
+      </form>
     </div>
   )
 }
