73c5e1ba2194947c298457da873cfd8c9ce02e01
Joseph Venable <JJVenable@gmail.com>
2021-12-23 12:07:24 -0500

    pausing uploading everything before presentations

--- frontEnd/src/components/Home.jsx
+++ frontEnd/src/components/Home.jsx
@@ -8,7 +8,6 @@ function Home(props) {
       <div className='homeDiv'>
         <h2>Welcome to my second project!</h2>
         <h3>This is a website for a solo property manager, or a multihome renter.</h3>
-        {/* <button><Link to='https://github.com/JJVenable/PropertyManagement'>Go to GitHub Repo</Link></button> */}
       </div>
     </div>
   )
